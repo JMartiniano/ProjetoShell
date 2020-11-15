c7441c4d05d24b9948d537844e94e231eed6537e
JMartiniano <jose.martiniano01@gmail.com>
2020-11-15 17:01:38 -0300

    Atualizações de José

--- Explorador.sh
+++ Explorador.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Funções
+
 function dir () {
 	for i in $(ls);do
 		if [ -d ${i} ]; then
@@ -60,45 +62,60 @@ function executar () {
 	./${arq}
 }
 
+# Settings
+
+touch backupSettings.sh
+echo -e "#!/bin/bash\nmkdir Backup\nmkdir ./Backup/$(date '+%d.%m.%y')" > backupSettings.sh
+chmod u+x backupSettings.sh
+
+# Cabeçalho
+
+echo -e "\nEXPLORADOR DE ARQUIVOS - LINUX\nVersão 1.0.0\nUsuário corrente: $(whoami)\nData: $(date '+%d/%m/%y')\n"
+
 while true; 
 do
-	echo -e "\n"
-	echo "a) Arquios"
+	echo -e "--> Menu\n"
+	echo "a) Arquivos"
 	echo "b) Diretórios"
 	echo "c) Tudo"
 	echo "d) Fazer Backup para outra máquina na rede"
 	echo "e) Limpar tela"
-	echo "q) Sair"
+	echo -e "q) Sair\n"
 	read -p "Escolha uma opção: " opt
 
 	if [ ${opt} ==  a ]; then	
-		echo -e "\nOpção 'a' selecionada"
+		echo -e "\n--> Opção 'a' selecionada"
 		echo -e  "Mostrando arquivos locais\n"
 		arq
 
 	elif [ ${opt} == b ];then
-	       	echo -e "\nOpção 'b' escolhida"
+	       	echo -e "\n--> Opção 'b' selecionada"
 		echo -e "Mostrando diretórios locais\n"
 		dir 
 	
 	elif [ ${opt} == c ];then 
-	       	echo -e "\nOpção 'c' escolhida"
+	       	echo -e "\n--> Opção 'c' selecionada"
 		echo -e "Mostrando todos os arquivos e diretórios locais\n"
 		ls -a | tr ' ' '\n'
 
 	elif [ ${opt} == d ];then 
-		echo -e "\nOpção 'd' escolhida"
-		echo -e "Backup em rede\nCadastrando máquina de backup\nAperte q a qualquer momento para sair"
+		echo -e "\n--> Opção 'd' selecionada"
+		echo -e "\nBackup em rede\nCadastrando máquina de backup\nAperte q a qualquer momento para sair\n"
 		read -p "Usuário da máquina: " user
 		
 		if [ ${user} == q ];then
 			echo "Saindo do cadastro de backup"
 		else
-			read -p "Ip da máquina: " ip
-			echo -e "Instalando SSH para acesso"
-			apt-get install openssh-server
-			echo -e "Acessando máquina de backup"
+			read -p "Ip da máquina: " ip			
+			echo -e "\nInstalando SSH para acesso"
+			apt-get install openssh-server &>> /dev/null
+			scp ./backupSettings.sh ${user}@${ip}:/home/projeto
+			echo -e "\nTutorial:\n[1] Digite a senha do usuário\n[2] Use o comando ./backupSettngs.sh para executar o arquivo backupSettings.sh\n[3] Digite exit e aperte enter para sair da máquina\n[5] Ao sair aperte enter para continuar o backup.\n"
 			ssh ${user}@${ip}
+			echo -e "\nCopiando arquivos locais para máquina remota ${user}\n"
+			data=$(date '+%d.%m.%y')
+			scp ./* ${user}@${ip}:/home/${user}/Backup/${data} && echo -e "\nBackup concluído no caminho: /home/${user}/backup/${data}" || echo -e "\nAlgo deu errado, tente novamente!"
+			
 		fi
 	elif [ ${opt} == e ];then
 		clear

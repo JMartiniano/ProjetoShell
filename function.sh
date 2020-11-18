2d8c89635e6b156e44066e8c3e549088b27a85e6
Bruno Ravi <bruno.ravi25@gmail.com>
2020-11-17 22:07:23 -0300

    Edição do menu.sh e adição de funções do function.sh

--- function.sh
+++ function.sh
@@ -54,3 +54,51 @@ function executar () {
 	./${arq}
 }
 
+function arqexe () {
+	ls -F | grep -E '[*]'
+}
+
+function atualdir () {
+	pwd
+}
+	 
+function previdir () {
+	cd ../
+	ls
+}
+
+function entrardir () {
+	(Chamar função listar diretórios)
+	read -p "Digite o nome do diretório que deseja entrar" dir
+	cd ./$dir
+	ls
+}
+
+function pesquisa () {
+	while true;
+	do
+		echo "a) Diretório"
+		echo "b) Arquivo"
+		echo "q) Sair"
+		read -p "Escolha uma opção: " opt
+		case $opt in
+			"a") 
+				read -p "Digite o diretório que deseja buscar: " dir
+				busca=$(find /home -name $dir)
+				if [ -z $busca ]
+				then
+					echo "Este diretório não existe"
+					exit 0
+				fi  ;;
+			"b")
+			       read -p "Digite o arquivo que deseja buscar: " arq
+		       		busca=$(find /home -namr $arq)
+		 		if [ -z $busca ]
+				then
+					echo "Este arquivo não existe"
+					exit 0		
+				fi ;;
+			"q") break ;;
+		esac
+	done
+}
--- menu.sh
+++ menu.sh
@@ -2,29 +2,17 @@
 
 while true;
 do
-	echo "a) Arquios"
+	echo "a) Arquivos"
 	echo "b) Diretórios"
 	echo "c) Tudo"
 	echo "d) Opção d"
 	echo "q) Sair"
 	read -p "Escolha uma opção: " opt
-	
-	if [ $((  $opt ==  "a" )) ]; then 
-		echo "Opção 'a' selecionada"
-	fi
-
-	if [ $(( $opt == "b")) ];then
-	       	echo "Opção 'b' escolhida"
-	if
-	
-	if [ $(( $opt == "c")) ];then 
-	       	echo "Opção 'c' escolhida"
-	fi
-
-	fi [ $(( $opt == "d" )) ];then 
-		echo "Opção 'd' escolhida"
-	if
-
-	if [ $(( $opt ==  "q" )) ];then
-       		break
-	fi
+	case $opt in
+		"a") echo "Arquivos" ;;
+		"b") echo "Diretórios" ;;
+		"c") echo "Tudo" ;;
+		"d") echo "Opção 'd' escolhida" ;;
+		"q") break ;;
+	esac
+done
